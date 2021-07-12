f2f788322ab8f29a41503d530487603a92bd16af
Boris Alarcon <boris.alarco@gmail.com>
2021-07-11 21:44:01 -0400

    registro creado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+import { LoginButton } from './components/Login';
+import { LogoutButton } from './components/Logout';
+import { Profile } from './components/Profile';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // hacemos la validacion usando auth0
+  const {isAuthenticated} = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isAuthenticated ? (
+          <> 
+            <Profile />
+            <LogoutButton/>        
+          </>
+         ) : (
+          <LoginButton /> )}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* almacena el estado de autenticacion de los usuarios */}
+    {/* provee metodos para logear/deslogear usuarios */}
+    <Auth0Provider 
+      domain="dev-gtpq18hv.us.auth0.com" 
+      clientId="lZedV56JLhVoVPOqhUzxlwjmXYUaeoHi" 
+      redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
